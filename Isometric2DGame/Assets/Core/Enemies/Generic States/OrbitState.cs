94c9ce64d3d445f234ad7e2202f3215563306261
Rynn <ryanoc0202@gmail.com>
2025-02-27 16:05:05 +0000

    [AI] Developed an Orbit state when the enemy is close

--- Isometric2DGame/Assets/Core/Enemies/Generic States/OrbitState.cs
+++ Isometric2DGame/Assets/Core/Enemies/Generic States/OrbitState.cs
@@ -2,18 +2,39 @@ using UnityEngine;
 
 public class OrbitState : State
 {
+    private float orbitDir = 1;
+    private float angle = 0;
+    
     public override void EnterState(Enemy enemy)
     {
-        throw new System.NotImplementedException();
+        orbitDir = Random.Range(0f, 1f);
+        orbitDir = orbitDir == 0 ? -1 : 1;
+        
+        angle = Vector2.Angle(enemy.transform.position, enemy.aggroTarget.transform.position);
     }
 
     public override void UpdateState(Enemy enemy)
     {
-        throw new System.NotImplementedException();
+        
+        if ((enemy.aggroTarget.transform.position - enemy.transform.position).magnitude >= enemy.orbitDistance * 2)
+        {
+            enemy.SwitchState(enemy.chaseState);
+        }
+        
+        angle += Time.deltaTime * orbitDir * (enemy.speed / 3);
+        
+        float x = enemy.aggroTarget.transform.position.x + Mathf.Cos(angle) * enemy.orbitDistance;
+        float y = enemy.aggroTarget.transform.position.y + Mathf.Sin(angle) * enemy.orbitDistance;
+        
+        Vector3 target = new Vector2(x, y);
+        
+        Vector2 direction = target - enemy.transform.position;
+        direction.Normalize();
+        
+        enemy.moveCharacter.Move(enemy.rb, direction , (enemy.speed / 3));
     }
 
     public override void ExitState(Enemy enemy)
     {
-        throw new System.NotImplementedException();
     }
 }
