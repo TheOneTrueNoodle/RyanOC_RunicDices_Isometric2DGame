2d4b5db7b43444d87904ca1f73937299164ca463
Rynn <ryanoc0202@gmail.com>
2025-02-27 13:03:56 +0000

    [Movement] Added the movement modifiers script and added the Mud movement modifier

--- Isometric2DGame/Assets/Core/Character Generic/MoveCharacter.cs
+++ Isometric2DGame/Assets/Core/Character Generic/MoveCharacter.cs
@@ -1,9 +1,37 @@
+using System;
 using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour
 {
+    // Variables to manage what kind of terrain a Character is walking on
+    private float modifier = 1;
+    
+    // A function that moves a rigidbody
     public void Move(Rigidbody2D rb, Vector2 dir, float speed)
     {
-        rb.linearVelocity = dir * speed;
+        rb.linearVelocity = dir * (speed * modifier);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collider has entered");
+        
+        other.TryGetComponent<MoveModifier>(out MoveModifier moveModifier);
+        switch (moveModifier.groundType)
+        {
+            case GroundType.mud:
+                modifier = 0.5f;
+                break;
+            case GroundType.ice:
+                break;
+            case GroundType.none:
+                modifier = 1;
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        modifier = 1;
     }
 }
