dc82b484bf94c977767ec89fc9e3319bd269f018
Rynn <ryanoc0202@gmail.com>
2025-03-01 16:24:53 +0000

    [Combat] Setting up framework for combat

--- Isometric2DGame/Assets/Core/Character Generic/HitBox.cs
+++ Isometric2DGame/Assets/Core/Character Generic/HitBox.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class HitBox : MonoBehaviour
 {
+    public float damage;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
